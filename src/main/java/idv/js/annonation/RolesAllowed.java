ab2730019362888b94fbf2da5d077aea7479edf7
zack <zack.j.hung@gmail.com>
2026-02-08 23:37:16 +0800

    簡單annonation宣告

---
+++ src/main/java/idv/js/annonation/RolesAllowed.java
@@ -0,0 +1,12 @@
+package idv.js.annonation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface RolesAllowed {
+  String value() default "User";
+}
